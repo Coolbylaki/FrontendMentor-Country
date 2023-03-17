e81eb9c1ddaf0227897432df1abf580ea084c89b
Lazar Stojanović <lakisd98@gmail.com>
2023-03-17 15:19:14 +0100

    Add: modular button component

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,10 +1,12 @@
+import Button from "../UI/Button";
+
 const Header = (props) => {
 	return (
 		<nav className="flex justify-between bg-white py-7 px-4 pc:px-12 pc:py-5 drop-shadow-sm">
 			<h1 className="pc:text-xl font-extrabold">Where in the world?</h1>
-			<button className="font-semibold" onClick={props.onChangeTheme}>
+			<Button className="font-semibold pc:hover:text-blue-900" onClick={props.onChangeTheme}>
 				<i className="fa-regular fa-moon pr-2"></i>Dark mode
-			</button>
+			</Button>
 		</nav>
 	);
 };

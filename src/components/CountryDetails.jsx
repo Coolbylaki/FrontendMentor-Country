a3d10aba8cefd894bb3c48424a43d914ce37f2c6
Lazar Stojanović <lakisd98@gmail.com>
2023-04-26 20:12:09 +0200

    Add: country details back button

--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -1,12 +1,20 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import countryData from "../assets/data.json";
+import Button from "./UI/Button";
 
 const CountryDetails = () => {
 	const params = useParams();
 
 	const targetCountry = countryData.find((country) => country.name === params.countryId);
 
-	return <h1>Country name: {targetCountry.name}</h1>;
+	return (
+		<main className="dark:bg-VDBlueDark min-h-screen p-7">
+			<Button className="dark:bg-DBlueDark dark:text-white py-1 px-7 mt-2 bg-white shadow-md rounded-sm">
+				<i class="fa-solid fa-arrow-left mr-3"></i>
+				<Link to="..">Back</Link>
+			</Button>
+		</main>
+	);
 };
 
 export default CountryDetails;
